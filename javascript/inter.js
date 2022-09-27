82120dc2cb23bac493d46e9946b5abcf7bf8fe48
Michel Fernando Azarate <107854976+MichelAzarate@users.noreply.github.com>
2022-09-27 09:45:58 -0700

    tar

--- javascript/inter.js
+++ javascript/inter.js
@@ -4,32 +4,54 @@
  var imgAtual;
  var maximg;
  var tmp;
+ var tempotroca
+ var vtempo;
+ var vload;
+ var vbarra
  function preCarregamento(){
-     var s=1;
+     let s=1;
      for(var i=0 ; i<4; i++){
          imgs[i]= new Image();
          imgs[i].src="imagens/s"+s+".jpg";
+         imgs[i].style.width="s"
          s++;
      }
  }
- function carregarimg(img){
-     slider.style.backgroundImage="url('"+imgs[img].src+"')";
- }
-
  function inicia(){
      preCarregamento();
      imgAtual=0;
      maximg=imgs.length-1;
      slider=document.getElementById("dvslider");
+     vbarra = document.getElementById("dvbarra");
      carregarimg(imgAtual);
-     tmp=setInterval(troca,3000);
+     tempotroca=0
+     animac();
  }
- function troca(){
-     imgAtual++;
-     if(imgAtual>maximg){
+ function carregarimg(img){
+    slider.style.backgroundImage="url('"+imgs[img].src+"')";
+    slider.style.backgroundSize="cover";
+    slider.style.backgroundRepeat="no-repeat";
+    // slider.style.backgroundAttachment="fixed";
+    slider.style.backgroundPosittion="center";
+}
+ function troca(dir){
+    tempotroca=0;
+    imgAtual+=dir;
+    if(imgAtual>maximg){
          imgAtual=0;
-     }
-     carregarimg(imgAtual);
-     
+     }else if(imgAtual<0){
+        imgAtual=maximg;
+    }
+     carregarimg(imgAtual);   
+ }
+ function animac(){
+    tempotroca++
+    if(tempotroca>=300){
+        tempotroca=0;
+        troca(1);
+    }
+    vtempo=tempotroca/3;
+    vbarra.style.width=vtempo+"%";
+    window.requestAnimationFrame(animac);
  }
  window.addEventListener("load",inicia);
